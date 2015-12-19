67f45c31eee7268aeb314f0f8da94452908a2e5f
Andrés Pinzón <andresmaopinzon@gmail.com>
2015-12-18 22:43:48 -0500

    correccion de css rotos

--- resources/views/auth/css.blade.php
+++ resources/views/auth/css.blade.php
@@ -16,13 +16,13 @@
 <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
 
 <!-- Related styles of various icon packs and plugins -->
-<link rel="stylesheet" href="/assets/css/plugins.min.css">
+<link rel="stylesheet" href="/assets/css/plugins.css">
 
 <!-- The main stylesheet of this template. All Bootstrap overwrites are defined in here -->
-<link rel="stylesheet" href="/assets/css/main.min.css">
+<link rel="stylesheet" href="/assets/css/main.css">
 
 <!-- Include a specific file here from css/themes/ folder to alter the default theme of the template -->
 
 <!-- The themes stylesheet of this template (for using specific theme color in individual elements - must included last) -->
-<link rel="stylesheet" href="/assets/css/themes/flat.min.css">
+<link rel="stylesheet" href="/assets/css/themes/flat.css">
 <!-- END Stylesheets -->
