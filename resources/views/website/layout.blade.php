04a8e04ac500a69a25c5e308871bedcde08b8104
Andrés Pinzón <andresmaopinzon@gmail.com>
2015-12-20 10:53:47 -0500

    google analitycs

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es" @yield('webPageScope', 'itemscope itemtype="http://schema.org/WebSite"')>
+<html lang="es" xml:lang="es" xmlns="http://www.w3.org/1999/xhtml" @yield('webPageScope', 'itemscope itemtype="http://schema.org/WebSite"')>
 	<head>
 		<meta charset="UTF-8">
 		<title>@yield('title', 'tufoto - Fotografía para toda la Vida - Fotografía Profesional')</title>
@@ -90,6 +90,17 @@
 			  }
 			  count ++;
 			});
+		</script>
+
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-71622518-1', 'auto');
+		  ga('send', 'pageview');
+
 		</script>
 
 		@yield('extra-js')
