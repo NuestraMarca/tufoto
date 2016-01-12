682508961e9f08dd8a4398f1cc3eeed24e479e97
Andrés Pinzón <andresmaopinzon@gmail.com>
2016-01-11 19:31:08 -0500

    estilos en imagenes de categorias

--- resources/views/website/gallery/categories.blade.php
+++ resources/views/website/gallery/categories.blade.php
@@ -11,19 +11,23 @@
 @endsection
 
 @section('content')
-	<h1 class="section-title title-md">Te presentamos nuestras galerías</h1>
-	<p class="section-description">Acá encontrarás lo mejor de nuestro trabajo fotográfico, tus recuerdos!</p>
-	
-	<section class="cover-gallery">
-		@foreach($categories as $category)
-			<a href=" {{ $category->complete_url }}">
-				<figure class="cover-photo">
-					<img src="{{ $category->cover }}" alt="{{ $category->alt }}">
-					<h2 class="cover-text">{{ $category->title_plural }}</h2>
-					<p class="cover-description"> {!! $category->description !!} </p>
-				</figure>
-			</a>
-		@endforeach
-	</section>
+	<article id="categories">
+		<h1 class="section-title title-md">Te presentamos nuestras galerías</h1>
+		<p class="section-description">Acá encontrarás lo mejor de nuestro trabajo fotográfico, lo que queremos conservar para toda la vida, tus recuerdos</p>
+		
+		<section class="cover-gallery">
+			@foreach($categories as $category)
+				<a href=" {{ $category->complete_url }}">
+					<figure class="cover-photo">
+						<div class="content-cover">
+							<img src="{{ $category->cover }}" alt="{{ $category->alt }}">
+							<h2 class="cover-text">{{ $category->title_plural }}</h2>
+						</div>
+						<p class="cover-description"> {!! $category->description !!} </p>
+					</figure>
+				</a>
+			@endforeach
+		</section>
+	</article>
 	
 @endsection
