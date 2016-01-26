b8b54a1b0d1a4e3957aee1134c1b90624fc85d88
Andrés Pinzón <andresmaopinzon@gmail.com>
2016-01-26 16:36:33 -0500

    pixel fb

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -14,7 +14,22 @@
 
  		<link rel="alternate" hreflang="es-co" href="/"> 
 		<link rel="stylesheet" type="text/css" href="/css/website/website.min.css">
-		
+			
+		<!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+		fbq('init', '346682378835818');
+		fbq('track', "PageView");</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=346682378835818&ev=PageView&noscript=1"
+		/></noscript>
+		<!-- End Facebook Pixel Code -->
+
 		@yield('extra-css')	
 	</head>
 	
