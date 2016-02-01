e0342a1bcdd63edd7a753b8ad2c94fdc739a4550
Andrés Pinzón <andresmaopinzon@gmail.com>
2016-02-01 13:54:22 -0500

    seguimiento conversiones

--- resources/views/website/contact.blade.php
+++ resources/views/website/contact.blade.php
@@ -67,4 +67,24 @@
 		  monthNamesShort: [ "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic" ],
 		});
 	</script>
+
+	<!-- Google Code for Ingreso a la p&aacute;gina de contacto Conversion Page -->
+	<script type="text/javascript">
+	/* <![CDATA[ */
+	var google_conversion_id = 1009818695;
+	var google_conversion_language = "en";
+	var google_conversion_format = "3";
+	var google_conversion_color = "ffffff";
+	var google_conversion_label = "iFyHCIKmwGMQx7jC4QM";
+	var google_remarketing_only = false;
+	/* ]]> */
+	</script>
+	<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+	</script>
+	<noscript>
+	<div style="display:inline;">
+	<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/1009818695/?label=iFyHCIKmwGMQx7jC4QM&amp;guid=ON&amp;script=0"/>
+	</div>
+	</noscript>
+
 @endsection
